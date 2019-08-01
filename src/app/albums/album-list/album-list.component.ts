0fca0c1c8dc382e9e6bdf7a4c2e24df2a5ff64e8
ramsaccount <tet@test.com>
2019-08-01 13:14:16 -0400

    changes

---
+++ src/app/albums/album-list/album-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+
+import { AlbumService } from "../shared/album.service";
+import { Album } from "../album.model";
+
+@Component({
+  selector: "app-album-list",
+  templateUrl: "./album-list.component.html",
+  styleUrls: ["./album-list.component.css"]
+})
+export class AlbumListComponent implements OnInit {
+  albumsArray: Album[];
+
+  constructor(private albumService: AlbumService) { }
+
+  ngOnInit(): void {
+    this.getAlbums();
+  }
+
+  parentFunctionHandler(album) {
+    alert('Album ' + album.albumName + ' was sent from the album card component');
+  }
+
+  getAlbums() {
+    this.albumsArray = this.albumService.getAlbums();
+  }
+}
